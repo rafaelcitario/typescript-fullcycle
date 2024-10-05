d0eb487e2d95e3813316c057acd82ca8672a477f
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-10-05 17:21:14 -0300

    modeling rich entity DDD

--- src/domain/category.entity.ts
+++ src/domain/category.entity.ts
@@ -1,4 +1,3 @@
-import { randomUUID } from 'node:crypto'
 export type CategoryConstructorProps = {
   category_id?: string
   name: string
@@ -12,15 +11,15 @@ export type CategoryCreateCommand = {
   is_active?: boolean
 }
 export class Category {
-  public category_id: string
-  public name: string
-  public description: string | null
-  public is_active: boolean
-  public created_at: Date
+  private category_id: string
+  private name: string
+  private description: string | null
+  private is_active: boolean
+  private created_at: Date
 
   constructor (props: CategoryConstructorProps) {
     const { category_id, name, description, is_active, created_at } = props
-    this.category_id = category_id ?? randomUUID()
+    this.category_id = category_id
     this.name = name
     this.description = description ?? null
     this.is_active = is_active ?? true
@@ -29,5 +28,31 @@ export class Category {
 
   static create (props: CategoryCreateCommand): Category {
     return new Category(props)
+  }
+
+  public changeName (name: string): void {
+    this.name = name
+  }
+
+  public changeDescription (description: string): void {
+    this.description = description
+  }
+
+  public activate (): void {
+    this.is_active = true
+  }
+
+  public deactivate (): void {
+    this.is_active = false
+  }
+
+  public toJSON (): Object {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at
+    }
   }
 }
