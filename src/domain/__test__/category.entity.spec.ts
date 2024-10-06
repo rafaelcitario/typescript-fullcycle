bb0f7225bc454d29e5cd6fb38df3834c13801929
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-10-06 01:37:57 -0300

    implements some test

--- src/domain/__test__/category.entity.spec.ts
+++ src/domain/__test__/category.entity.spec.ts
@@ -1,123 +1,91 @@
-import { Category } from "../category.entity";
+import { Category } from "../category.entity"
 describe("Category Entity Unit Tests", () => {
   // suite test 1
-<<<<<<< HEAD
-
   describe("Create a new category using constructor", () => {
-    describe("it should test constructor", () => {
-      it("should create a category with default values", () => {
-=======
-<<<<<<< HEAD
-  describe("Create a new category using constructor", () => {
-=======
-  describe("it should test constructor", () => {
->>>>>>> 3f1972289e42a30e70b22c22cc833dc72f606832
     it("should create a category with default values", () => {
->>>>>>> f5e061337c1681245aebf0909e142520f60601ff
       // Arrange
-        const arrange: Object = {}
+      const arrange: Object = {}
 
-        // Act
-        let category = new Category({
-          name: "Movie",
-        })
-
-        // Assert
-        expect(category.category_id).toBeUndefined()
-        expect(category.name).toBe("Movie")
-        expect(category.description).toBeNull()
-        expect(category.is_active).toBeTruthy()
-        expect(category.created_at).toBeInstanceOf(Date)
+      // Act
+      let category = new Category({
+        name: "Movie",
       })
 
-      it("should create another category with different values", () => {
-        const created_at = new Date()
-        const category = new Category({
-          category_id: "1",
-          name: "Drama Movie",
-          description: "Drama Movie Description",
-          is_active: false,
-          created_at,
-        })
-
-<<<<<<< HEAD
-        expect(category.category_id).not.toBeUndefined()
-        expect(category.name).toBe("Drama Movie")
-        expect(category.description).toBe("Drama Movie Description")
-        expect(category.is_active).toBeFalsy()
-        expect(category.created_at).toEqual(created_at)
-      })
+      // Assert
+      expect(category.category_id).toBeUndefined()
+      expect(category.name).toBe("Movie")
+      expect(category.description).toBeNull()
+      expect(category.is_active).toBeTruthy()
+      expect(category.created_at).toBeInstanceOf(Date)
     })
-    // suite test 2
-    describe("Create a new category with create command", () => {
-      it("should create a new category with name only", () => {
-        const category = Category.create({
-          name: "Terror Movie",
-        })
 
-        expect(category.category_id).toBeUndefined()
-        expect(category.name).toStrictEqual("Terror Movie")
-        expect(category.description).toBeNull()
-        expect(category.is_active).toBeTruthy()
-        expect(category.created_at).toBeInstanceOf(Date)
+    it("should create another category with different values", () => {
+      const created_at = new Date()
+      const category = new Category({
+        category_id: "1",
+        name: "Drama Movie",
+        description: "Drama Movie Description",
+        is_active: false,
+        created_at,
       })
+
+      expect(category.category_id).not.toBeUndefined()
+      expect(category.name).toBe("Drama Movie")
+      expect(category.description).toBe("Drama Movie Description")
+      expect(category.is_active).toBeFalsy()
+      expect(category.created_at).toEqual(created_at)
     })
-    // suite test 3
-    describe("Create a new category and change values", () => {
-=======
-      expect(category.category_id).not.toBeUndefined();
-      expect(category.name).toBe("Drama Movie");
-      expect(category.description).toBe("Drama Movie Description");
-      expect(category.is_active).toBeFalsy();
-      expect(category.created_at).toEqual(created_at);
-    });
-  });
-<<<<<<< HEAD
+  })
   // suite test 2
   describe("Create a new category with create command", () => {
     it("should create a new category with name only", () => {
->>>>>>> f5e061337c1681245aebf0909e142520f60601ff
       const category = Category.create({
-        name: "Comedy Movie",
-        description: "This is a funny movie",
+        name: "Terror Movie",
       })
 
-      it("should change name of the category", () => {
-        category.changeName("Comedy Cats Movie")
-        expect(category.name).toBe("Comedy Cats Movie")
-        expect(category.description).toStrictEqual("This is a funny movie")
-      })
-
-      it("should change description of the category", () => {
-        category.changeDescription("The cats is so funny")
-        expect(category.name).toBe("Comedy Cats Movie")
-        expect(category.description).toStrictEqual("The cats is so funny")
-      })
+      expect(category.category_id).toBeUndefined()
+      expect(category.name).toStrictEqual("Terror Movie")
+      expect(category.description).toBeNull()
+      expect(category.is_active).toBeTruthy()
+      expect(category.created_at).toBeInstanceOf(Date)
     })
-    // suite test 4
-    describe("Create a new category and return a JSON", () => {
-      it("should return a JSON from category values", () => {
-        const created_at = new Date()
-        const category = new Category({
-          name: "Dogs vs Cats",
-          description: 'This movies is "Animalic"',
-          created_at,
-        })
-        const categoryJsonValue = category.toJSON()
-        expect(categoryJsonValue).toMatchObject({
-          category_id: undefined,
-          name: "Dogs vs Cats",
-          description: 'This movies is "Animalic"',
-          is_active: true,
-          created_at,
-        })
+  })
+  // suite test 3
+  describe("Create a new category and change values", () => {
+    const category = Category.create({
+      name: "Comedy Movie",
+      description: "This is a funny movie",
+    })
+
+    it("should change name of the category", () => {
+      category.changeName("Comedy Cats Movie")
+      expect(category.name).toBe("Comedy Cats Movie")
+      expect(category.description).toStrictEqual("This is a funny movie")
+    })
+
+    it("should change description of the category", () => {
+      category.changeDescription("The cats is so funny")
+      expect(category.name).toBe("Comedy Cats Movie")
+      expect(category.description).toStrictEqual("The cats is so funny")
+    })
+  })
+  // suite test 4
+  describe("Create a new category and return a JSON", () => {
+    it("should return a JSON from category values", () => {
+      const created_at = new Date()
+      const category = new Category({
+        name: "Dogs vs Cats",
+        description: 'This movies is "Animalic"',
+        created_at,
+      })
+      const categoryJsonValue = category.toJSON()
+      expect(categoryJsonValue).toMatchObject({
+        category_id: undefined,
+        name: "Dogs vs Cats",
+        description: 'This movies is "Animalic"',
+        is_active: true,
+        created_at,
       })
     })
   })
-<<<<<<< HEAD
 })
-=======
-=======
->>>>>>> 3f1972289e42a30e70b22c22cc833dc72f606832
-});
->>>>>>> f5e061337c1681245aebf0909e142520f60601ff
